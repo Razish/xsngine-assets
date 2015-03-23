58042f0d4d187cf161ae360051af3bfebf834d8e
Razish <mrrazish@gmail.com>
2015-03-23 20:40:01 +1100

    Updated assets

--- shaders/f_model.glsl
+++ shaders/f_model.glsl
@@ -2,10 +2,10 @@
 
 uniform sampler2D u_Texture;
 
-//in vec2 texCoord;
+in vec2 texCoord;
 
 out vec4 out_FragColor;
 
 void main() {
-	out_FragColor = vec4( 1.0 );//vec4( texture( u_Texture, texCoord ).rgb, 1.0 );
+	out_FragColor = vec4( texture( u_Texture, texCoord ).rgb, 1.0 );
 }
--- shaders/v_model.glsl
+++ shaders/v_model.glsl
@@ -5,13 +5,13 @@ uniform PerFrame {
 };
 
 in vec3 in_Position;
-//in vec3 in_Normal;
-//in vec2 in_TexCoord;
+in vec3 in_Normal;
+in vec2 in_TexCoord;
 
-//out vec2 texCoord;
+out vec2 texCoord;
 
 void main() {
 	gl_Position = u_ProjectionMatrix * vec4( in_Position, 1.0 );
 
-//	texCoord = in_TexCoord;
+	texCoord = in_TexCoord;
 }
