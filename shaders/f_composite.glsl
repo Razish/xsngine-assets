3a1511f788d76d4f14725e721b4467a93d0a3bea
Razish <mrrazish@gmail.com>
2015-04-22 10:20:28 +1000

    Update composite shaders

--- shaders/f_composite.glsl
+++ shaders/f_composite.glsl
@@ -1,7 +1,6 @@
 #version 140
 
 uniform sampler2D u_sceneTexture;
-uniform sampler2D u_depthTexture;
 
 in vec2 texCoord;
 in vec4 colour;
@@ -9,5 +8,5 @@ in vec4 colour;
 out vec4 out_FragColor;
 
 void main() {
-	out_FragColor = vec4( texture( u_depthTexture, texCoord ).rgb, 1.0 );
+	out_FragColor = vec4( texture( u_sceneTexture, texCoord ).rgb, 1.0 );
 }
--- shaders/v_composite.glsl
+++ shaders/v_composite.glsl
@@ -1,9 +1,5 @@
 #version 140
 
-uniform PerFrame {
-	mat4 u_ProjectionMatrix;
-};
-
 in vec2 in_Position;
 in vec2 in_TexCoord;
 in vec4 in_Colour;
@@ -12,7 +8,7 @@ out vec2 texCoord;
 out vec4 colour;
 
 void main() {
-	gl_Position = u_ProjectionMatrix * vec4( in_Position, 0.0, 1.0 );
+	gl_Position = vec4( in_Position, 0.0, 1.0 );
 
 	texCoord = in_TexCoord;
 	colour = in_Colour;
