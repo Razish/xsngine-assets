8766506678988f22ce4eb8ff2eaa34788a79fcdc
Razish <mrrazish@gmail.com>
2015-05-19 15:04:20 +1000

    Update shaders to consider vertex normals. Added dirt texture for testing (authored by demolitiondan @ deviantart)

--- shaders/f_model.glsl
+++ shaders/f_model.glsl
@@ -2,10 +2,20 @@
 
 uniform sampler2D u_Texture;
 
+in vec3 normal;
 in vec2 texCoord;
 
 out vec4 out_FragColor;
 
 void main() {
-	out_FragColor = vec4( texture( u_Texture, texCoord ).rgb, 1.0 );
+	vec3 diffuse = texture( u_Texture, texCoord ).rgb;
+	float d = max(
+		0.0,
+		dot(
+			normalize( normal.xyz ),
+			vec3( 0.75, -1.0, 0.0 )
+		)
+	);
+	vec3 colour = diffuse * d;
+	out_FragColor = vec4( colour, 1.0 );
 }
--- shaders/v_model.glsl
+++ shaders/v_model.glsl
@@ -9,9 +9,11 @@ in vec3 in_Normal;
 in vec2 in_TexCoord;
 
 out vec2 texCoord;
+out vec3 normal;
 
 void main() {
 	gl_Position = u_ProjectionMatrix * vec4( in_Position, 1.0 );
 
 	texCoord = in_TexCoord;
+	normal = in_Normal;
 }
