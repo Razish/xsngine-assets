534902c2185ad4cf171d5a1a7c9494b9c839d685
Razish <mrrazish@gmail.com>
2015-05-21 16:11:14 +1000

    Compute ambient and directional lighting in "model" fragment shader. Add monkey and torus models Remove unused files Update menus

--- shaders/f_model.glsl
+++ shaders/f_model.glsl
@@ -8,14 +8,26 @@ in vec2 texCoord;
 out vec4 out_FragColor;
 
 void main() {
-	vec3 diffuse = texture( u_Texture, texCoord ).rgb;
-	float d = max(
+	vec3 textureColour = texture( u_Texture, texCoord ).rgb;
+
+	// global light source, i.e. the sun
+	vec3 directionalColour = vec3( 1.0, 0.5, 0.5 );
+	float directionalLight = max(
 		0.0,
 		dot(
 			normalize( normal.xyz ),
-			vec3( 0.75, -1.0, 0.0 )
+			vec3( 1.0, 1.0, 0.0 )
 		)
 	);
-	vec3 colour = diffuse * d;
+
+	vec3 ambientColour = vec3( 0.1, 0.1, 0.1 );
+	float ambientIntensity = 0.5;
+
+	vec3 colour =
+		// ambient
+		ambientColour * textureColour +
+		// directional
+		textureColour * directionalColour * directionalLight;
+
 	out_FragColor = vec4( colour, 1.0 );
 }
